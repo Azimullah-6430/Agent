cb54b82ed83f23c97cc11c6d6e06aa374aafdfb2
AZIMULLAH A <azim44667@gmail.com>
2026-09-08 11:34:41 +0530

    Create __init__.py

---
+++ app/youtube/__init__.py
@@ -0,0 +1,26 @@
+from Flask import Blueprint , request , jsonify
+
+youtube_bp = Blueprint(
+    " youtube ",
+    __name__
+    )
+
+@youtube_bp.route(
+  "/play"
+  methods = ["POST"]
+)
+
+def play():
+  data = request.get_json(
+    silent = True
+    ) or {}
+command = data.get(
+"command",
+""
+).strip()
+
+if not command :
+  return jsonify({
+    "success":"False",
+    "message":"There's no YouTube video mentioned"
+  })400
