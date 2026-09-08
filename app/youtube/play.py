39a8f92c39d491e69fb9260fc1e357af621df8f6
AZIMULLAH A <azim44667@gmail.com>
2026-09-08 14:31:37 +0530

    Refactor YouTube video ID extraction and URL generation

--- app/youtube/play.py
+++ app/youtube/play.py
@@ -47,4 +47,19 @@ def get_vid(query):
       text
     )
 
-          
+    if match:
+      query = match.group(1)
+      break
+       
+  query = query.strip()
+
+  video_id = get_vid(query)
+
+  if not video_id:
+    return None
+
+  return (
+    "https://www.youtube.com/embed/"
+    + video_id
+    + "?autoplay=1&mute=0"
+  )
