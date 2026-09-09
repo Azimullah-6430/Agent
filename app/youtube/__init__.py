007ca2ce92931d9c89aec9011f2bd55ec187628c
AZIMULLAH A <azim44667@gmail.com>
2026-09-09 10:08:07 +0530

    Update __init__.py

--- app/youtube/__init__.py
+++ app/youtube/__init__.py
@@ -1,26 +1,48 @@
-from Flask import Blueprint , request , jsonify
+from flask import Blueprint, request, jsonify
+
+from app.youtube.player import create_youtube_url
 
 youtube_bp = Blueprint(
-    " youtube ",
+    "youtube",
     __name__
-    )
-
-@youtube_bp.route(
-  "/play"
-  methods = ["POST"]
 )
 
+@youtube_bp.route(
+    "/play",
+    methods=["POST"]
+)
 def play():
-  data = request.get_json(
-    silent = True
-    ) or {}
-command = data.get(
-"command",
-""
-).strip()
 
-if not command :
-  return jsonify({
-    "success":"False",
-    "message":"There's no YouTube video mentioned"
-  })400
+    data = request.get_json(
+        silent=True
+    ) or {}
+
+    command = data.get(
+        "command",
+        ""
+    ).strip()
+
+    if not command:
+
+        return jsonify({
+            "success": False,
+            "message": "Song name is required"
+        }), 400
+
+    url = create_youtube_url(
+        command
+    )
+
+    if not url:
+
+        return jsonify({
+            "success": False,
+            "message": "Could not find the song"
+        }), 404
+
+    return jsonify({
+        "success": True,
+        "type": "youtube",
+        "query": command,
+        "url": url
+    })
